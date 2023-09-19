fe0191e01efe2c4c78bc714b3c5bc3e22c15a2c1
g7enov <glebsem04@gmail.com>
2023-09-19 14:06:51 +0300

    commit1

---
+++ src/ru/mirea/lab2/task5/DogKennel.java
@@ -0,0 +1,14 @@
+package ru.mirea.lab2.task5;
+import java.util.Scanner;
+import ru.mirea.lab2.task5.Dog;
+
+public class DogKennel {
+    private Dog[] kennel;
+
+    public void add_dog(Dog dog){
+    }
+    public static void main(String[] args){
+
+    }
+}
+
